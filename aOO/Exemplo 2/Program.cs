c2e1fe8b048debbfa19c622252d0c08b5de31c88
nananagen <137368966+nananagen@users.noreply.github.com>
2023-08-18 19:50:41 -0300

    Update Program.cs

--- aOO/Exemplo 2/Program.cs
+++ aOO/Exemplo 2/Program.cs
@@ -1,7 +1,20 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
-    {
+    static void Main(string[] args){
+    /* Func<int, int, int> SomaNumeros = (int a, int b) => a + b;
+        Func<int, int, int> SomaNumeros = (int a, int b, int c) => a + b + c; */
         
-}
+    Console.WriteLine (Somar(1,2));
+    Console.WriteLine(Somar(1,2,3));
+    }
+
+     //sobrecarga
+     static int Somar (int a, int b)
+     {
+        return a + b;
+     }
+     static int Somar (int a, int b, int c)
+     {
+         return a + b + c;
+     }
 }
