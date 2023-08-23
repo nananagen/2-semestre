e342adf686140c90fa2ebb70c234c1d80d73e5b6
nananagen <137368966+nananagen@users.noreply.github.com>
2023-08-22 21:38:35 -0300

    Update Program.cs

--- esdad/aula18-08/Program.cs
+++ esdad/aula18-08/Program.cs
@@ -1,4 +1,4 @@
-int inicio = 1, fim = 5;
+/* int inicio = 1, fim = 5;
 
 Console.WriteLine($"Soma dos pares entre {inicio} e {fim}: {SomaPares(inicio, fim)}.");
 
@@ -13,4 +13,42 @@ int SomaPares(int primeiro, int ultimo, int soma = 0)
         soma += num;
     
     return SomaPares(num + 1, ultimo, soma);
-}
+} */
+
+//or
+
+/* int inicio = 1, fim = 5;
+
+Console.WriteLine($"Soma dos pares entre {inicio} e {fim}: {SomaPares(inicio, fim)}.");
+
+int SomaPares(int primeiro, int ultimo)
+{
+    int num = primeiro;
+
+    if (num > ultimo)
+        return 0;
+    
+    if (num % 2 == 0)
+        return num + SomaPares(num + 1, ultimo);
+    else
+        return SomaPares(num + 1, ultimo);
+} */
+
+//or
+
+/* int inicio = 1, fim = 5;
+
+Console.WriteLine($"Soma dos pares entre {inicio} e {fim}: {SomaPares(inicio, fim)}.");
+
+int SomaPares(int primeiro, int ultimo)
+{
+    if (primeiro > ultimo)
+        return 0;
+    
+    if (primeiro % 2 == 0)
+        return primeiro + SomaPares(primeiro + 1, ultimo);
+    else
+        return SomaPares(primeiro + 1, ultimo);
+} */
+
+
