3f537739268c9b3a037bd2a76bbcb8ae5ff45353
nananagen <137368966+nananagen@users.noreply.github.com>
2023-09-05 20:18:46 -0300

    Add files via upload

---
+++ twa/topico 6/script.js
@@ -0,0 +1,7 @@
+let paragrafo = document.getElementsByTagName('p');
+let pImpar = document.getElementsByClassName('impar');
+let divPrincipal = document.getElementsByTagName('div')
+
+let selParagrafos = document.querySelector('#primeiro');
+let todosParagrafos = document.querySelectorAll('p');
+let selImpar = document.querySelectorAll('.impar');
