4c8fab9be7c33e231cc21d806fdf5db90d8e377d
nananagen <137368966+nananagen@users.noreply.github.com>
2023-09-14 20:21:50 -0300

    Update script.js

--- twa/12-09/script.js
+++ twa/12-09/script.js
@@ -7,3 +7,38 @@ let divPrincipal = document.getElementsByTagName('div')
 let selParagrafos = document.querySelector('#primeiro');
 let todosParagrafos = document.querySelectorAll('p');
 let selImpar = document.querySelectorAll('.impar');
+
+
+const corpo= document.querySelector('body'); //fora referencia todas as funções
+const botao = document.getElementById('botao');
+
+function modoEscuro() {
+    //const corpo= document.querySelector('body'); 
+    //dentro p referenciar esta função
+
+    if (botao.innerText == 'Modo Claro') {
+        corpo.classList.remove('escuro');
+        botao.innerText = "Modo Escuro";
+    } else {
+        corpo.classList.add('escuro');
+        botao.innerText = "Modo Claro";
+    }
+}
+
+function alterarModo() {
+    const textosVermelhos= document.querySelectorAll('.texto-vermelho');
+
+    if (textosVermelhos.length > 0) {
+        textosVermelhos.forEach( texto => {
+            texto.classList.replace('texto-vermelho', 'texto-azul');
+        });
+    }else {
+        const textosAzuis = document.querySelectorAll('.texto-azul')
+
+        textosAzuis.forEach( texto => {
+            texto.classList.replace('texto-azul', 'texto-vermelho');
+        });
+    }
+    
+    corpo.classList.toggle('escuro');
+}
