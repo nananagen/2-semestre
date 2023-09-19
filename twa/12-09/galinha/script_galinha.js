9f083c7177fce6f3a93b9780c012896cc7a95d1e
nananagen <137368966+nananagen@users.noreply.github.com>
2023-09-19 20:17:02 -0300

    Add files via upload

---
+++ twa/12-09/galinha/script_galinha.js
@@ -0,0 +1,8 @@
+let imagens =
+['../assets/images/bao.webp',
+'../assets/images/esquisito.jpg,',
+'../assets/images/familia.webp',
+'../assets/images/ruim.jpg'];
+
+const slide = document.getElementById('slide');
+slide.setAttribute('src', imagens[0]); //insere a primeira image da lista
