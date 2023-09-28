cf017514e4c43e1a249c60fc6da8d7f5000ce6da
nananagen <137368966+nananagen@users.noreply.github.com>
2023-09-28 19:47:02 -0300

    Add files via upload

---
+++ twa/topico6_2.0/script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('nav');
+
+const paragrafo = menu.querySelector('p');
+
+const conteudo = document.getElementById('conteudo');
+
+const primeiroLi = document.querySelector('nav ul li');
+
+const superior = primeiroLi.children[0];
+const inferior = primeiroLi.children[2];
+
+//tópico 6 - node
+
+//inferior.after(paragrafo);
+//primeiroLi.removeChild(superior);
+
+/*conteudo.appendChild(paragrafo);
+conteudo.appendChild(superior);
+conteudo.appendChild(inferior); */
